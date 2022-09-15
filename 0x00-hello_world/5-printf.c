54a372b022d46ec938d154f088d722fb2ebaf0a3
zgamble30 <you@example.com>
2022-09-15 08:56:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -Prints message to terminal using print f command
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

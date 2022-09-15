ddcc758f3b7e8f6852fedb8d45dade46fb7717ac
zgamble30 <you@example.com>
2022-09-15 08:52:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
- * main - Prints message to terminal
- * 
- * Return: 0 if succesful
+ * main -Prints message to terminal
+ *
+ * Return: 0 if successful
  */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 }

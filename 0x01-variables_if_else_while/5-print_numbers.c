7eb8c1873a84e389b44ab6d13222470e936f4dc4
zgamble30 <you@example.com>
2022-09-19 08:34:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - prints numbers 1-9
+ *
+ *
+ * Return:0 if runs successfuly
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}

bec5baf395eb7a067c8be0bac9786f3590138cd3
zgamble30 <you@example.com>
2022-09-19 08:52:04 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - prints all possible combinations of single digit numbers
+ *
+ *
+ * Return:0 if runs successfuly
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

385a877b3db622c55f960c7c777c487edeee6441
zgamble30 <you@example.com>
2022-09-19 08:24:37 -0700

    exctept e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - prints alphabet in lowercase except e and q
+ *
+ *
+ * Return:0 if runs successfuly
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			if ((c == 'e') || (c == 'q'))
+				continue;
+			putchar(c);
+		}
+		putchar('\n');
+		return (0);
+	}

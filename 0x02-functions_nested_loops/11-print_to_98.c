3486e6895c11e1282db10f9d09f17ab3c6693400
zgamble30 <you@example.com>
2022-09-20 13:58:31 -0700

    too many characters in description

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - prints all natural numbers from input to 98, in order seperated by a coma followed by a space.
+ * print_to_98 - prints all natural numbers from input to
+ * 98, in order seperated by a coma followed by a space.
  *
  * @n: the number to begin counting at.
  */

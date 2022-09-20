181db33bd73fffb623d0ac89bfb287cac618f5be
zgamble30 <you@example.com>
2022-09-20 11:18:48 -0700

    bug fix spacing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- *print_alphabet prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  *
  *Return: 0 if successful
  *
  */
-
 int print_alphabet(void)
 {
 	int c;

1e06842748ee404af7b02ee0a0681f8cc0b2d68a
zgamble30 <you@example.com>
2022-09-20 11:30:50 -0700

    _islower check to see if the input char is lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _putchar(char);
 int print_alphabet(void);
 int print_alphabet_x10(void);
-int _islower(int c);
+int _islower(char c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);

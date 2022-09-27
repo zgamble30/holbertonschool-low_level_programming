540fd0f62f7435f948709f036c01b0084d4baa1d
zgamble30 <you@example.com>
2022-09-26 19:13:31 -0700

    mul takes two numbers and returns the product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - takes two numbers and returns the product
+*@a: first number to be multiplied
+*@b: second number to be multiplied
+*
+*Return: the prodoct of the two given numbers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b)
+}

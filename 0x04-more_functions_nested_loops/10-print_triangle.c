808d48ecae6b051075af93bd7932efbbf1e99b34
zgamble30 <you@example.com>
2022-09-27 12:56:23 -0700

    triangle task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*print_tringle - prints triangle to console
+*@size: size of triangle
+*
+*/
+
+void print_triangle(int size)
+{
+	int i, j, s;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= (size - i); j++)
+			{
+				_putchar(' ');
+			}
+			for (s = 1; s <= i; s++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

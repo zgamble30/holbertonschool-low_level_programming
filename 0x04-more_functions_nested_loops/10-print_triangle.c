2843b2a8299fbcf662063705adc22803995d8ef0
zgamble30 <you@example.com>
2022-09-27 13:21:24 -0700

    task 10 bug

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,20 +7,29 @@
 
 void print_triangle(int size)
 {
-	int a, b;
+	int c, i, j;
+
+	c = 0;
+	i = size - 1;
+	while (c < size)
+	{
+		i = size - 1 - c;
+		j = c + 1;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		while (j > 0)
+		{
+			_putchar('#');
+			j--;
+		}
+		_putchar('\n');
+		c++;
+	}
 
 	if (size <= 0)
 		_putchar('\n');
-	for (a = 1; a <= size; a++)
-	{
-		for (b = 1; b <= size; b++)
-		{
-			if (b <= (size - a))
-				_putchar(' ');
-			else
-				_putchar('#');
-		}
-		_putchar('\n');
-	}
-
 }
+

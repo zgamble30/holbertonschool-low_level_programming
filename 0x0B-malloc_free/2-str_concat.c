55e8a57f848d34ae311eab1cf3a1d5640e17b34e
zgamble30 <you@example.com>
2022-10-10 20:33:34 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,29 +2,39 @@
 #include <stdlib.h>
 
 /**
- * _strdup - copy a string to a newly allocated space in memory
- * @str: string to copy
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: pointer to duplicated string or NULL if error
+ * Return: pointer to newly allocated space in memory, or NULL if error
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j;
+	unsigned int i, j, k, l;
 	char *s;
 
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; str[i]; i++)
-		;
-	i++;
-	s = malloc(i * sizeof(char));
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	s = malloc(k * sizeof(char));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
-	for (j = 0; j < i; j++)
-	{
-		s[j] = str[j];
-	}
+	for (l = 0; l < i; l++)
+		s[l] = s1[l];
+	for (l = 0; l < j; l++)
+		s[l + i] = s2[l];
+	s[i + j] = '\0';
 	return (s);
 }

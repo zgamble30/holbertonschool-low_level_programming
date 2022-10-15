483a5732de54846f77323438899aa80b88e2d0e3
zgamble30 <you@example.com>
2022-10-15 12:22:59 -0700

    task 1 bug fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,34 +9,33 @@
  *
  * Return: char pointer to newly allocated place in memory
  */
+
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k;
-	char *s;
+	char *newStr;
+	unsigned int i, j, lens1, lens2;
 
 	if (s1 == NULL)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
+		s2 = "";
+
+	lens1 = strlen(s1);
+	lens2 = strlen(s2);
+	if (n >= lens2)
+		n = lens2;
+	newStr = malloc(sizeof(char) * (lens1 + n + 1));
+	if (newStr == NULL)
 		return (NULL);
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + i] = s2[k];
-	s[i + j] = '\0';
-	return (s);
+
+	for (i = 0; i < lens1; i++)
+		newStr[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	{
+		newStr[i + j] = s2[j];
+	}
+	newStr[i + j] = '\0';
+	return (newStr);
 }

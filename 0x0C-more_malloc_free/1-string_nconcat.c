fe4dfcd30ed0d3ac9525601b6cf22eecd7289087
zgamble30 <you@example.com>
2022-10-15 12:26:06 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
-#include "main.h"/**
- * string_nconcat - concatenates two strings.
- * @s1: first string to copy
- * @s2: second string to copy
- * @n: number of bytes of s2 to copy
+
+/**
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * Return: char pointer to newly allocated place in memory
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *newStr;
-	unsigned int i, j, lens1, lens2;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	lens1 = strlen(s1);
-	lens2 = strlen(s2);
-	if (n >= lens2)
-		n = lens2;
-	newStr = malloc(sizeof(char) * (lens1 + n + 1));
-	if (newStr == NULL)
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
 
-	for (i = 0; i < lens1; i++)
-		newStr[i] = s1[i];
+	len = 0;
 
-	for (j = 0; j < n; j++)
-	{
-		newStr[i + j] = s2[j];
-	}
-	newStr[i + j] = '\0';
-	return (newStr);
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }

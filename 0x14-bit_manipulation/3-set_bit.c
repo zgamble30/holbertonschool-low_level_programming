1b192642a14fe771586d3f465cd5c44a4cd1f789
zgamble30 <you@example.com>
2022-11-08 13:59:33 -0800

    task 4-5

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * get_bit - get bit of n at  index
- * @n: number to get bit of
- * @index: index of bit to get
- * Return: value of the bit at the index or -1 if an error occured
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: number to set to 1
+ * @index: index of the value of a bit tp set to 1
+ * Return: 1 if it worked and -1 if it did not
  */
 
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	int bit;
-
 	if (index > 20)
 		return (-1);
-
-	bit = (n >> index) & 1;
-
-	return (bit);
+	*n |= (1 << index);
+	return (1);
 }
